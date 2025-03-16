d97ee25ce0cd3989ff8b6333fd981a885dbd2f58
SankalpKawade <kawadesankalp00@gmail.com>
2025-03-16 18:31:11 +0530

    updates

--- userdata/sonar-setup.sh
+++ userdata/sonar-setup.sh
@@ -12,18 +12,14 @@ sonarqube   -   nofile   65536
 sonarqube   -   nproc    409
 EOT
 
-
 sudo apt-get update -y
-sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-17-jdk -y
 sudo update-alternatives --config java
-
 
 java -version
 
-
 sudo apt update
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt install postgresql postgresql-contrib -y
@@ -40,10 +36,10 @@ systemctl restart  postgresql
 netstat -tulpena | grep postgres
 sudo mkdir -p /sonarqube/
 cd /sonarqube/
-sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.3.0.34182.zip
+sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.8.100196.zip
 sudo apt-get install zip -y
-sudo unzip -o sonarqube-8.3.0.34182.zip -d /opt/
-sudo mv /opt/sonarqube-8.3.0.34182/ /opt/sonarqube
+sudo unzip -o sonarqube-9.9.8.100196.zip -d /opt/
+sudo mv /opt/sonarqube-9.9.8.100196/ /opt/sonarqube
 sudo groupadd sonar
 sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
 sudo chown sonar:sonar /opt/sonarqube/ -R
@@ -60,36 +56,28 @@ sonar.log.level=INFO
 sonar.path.logs=logs
 EOT
 
-
 cat <<EOT> /etc/systemd/system/sonarqube.service
 [Unit]
 Description=SonarQube service
 After=syslog.target network.target
 
-
 [Service]
 Type=forking
 
-
 ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-
 
 User=sonar
 Group=sonar
 Restart=always
 
-
 LimitNOFILE=65536
 LimitNPROC=4096
-
-
 
 
 [Install]
 WantedBy=multi-user.target
 EOT
-
 
 systemctl daemon-reload
 systemctl enable sonarqube.service
@@ -103,20 +91,17 @@ server{
     listen      80;
     server_name sonarqube.groophy.in;
 
-
     access_log  /var/log/nginx/sonar.access.log;
     error_log   /var/log/nginx/sonar.error.log;
 
-
     proxy_buffers 16 64k;
     proxy_buffer_size 128k;
-
 
     location / {
         proxy_pass  http://127.0.0.1:9000;
         proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         proxy_redirect off;
-             
+              
         proxy_set_header    Host            \$host;
         proxy_set_header    X-Real-IP       \$remote_addr;
         proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -128,7 +113,6 @@ ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
 systemctl enable nginx.service
 #systemctl restart nginx.service
 sudo ufw allow 80,9000,9001/tcp
-
 
 echo "System reboot in 30 sec"
 sleep 30
